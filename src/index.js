6e0e0a387742c8dece234ad868724b4c51bb5c66
David Yin <davidyin92@hotmail.com>
2018-02-08 14:36:01 -0800

    Code along setup Game.js Board.js Paddle.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './styles/game.css';
 import Game from './partials/Game'
 
-// create a game instance
+// create a game instance, instantiating Game
 const game = new Game('game', 512, 256);
-
+// reference to div id=game, giving it width and height
 (function gameLoop() {
     game.render();
+    // console.log('something')
     requestAnimationFrame(gameLoop);
 })();
+// () immediately invoke function expression
+// requestAnimationFame method to get 60fps, if leave tab will not continue
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,61 @@
+import { SVG_NS, KEYS } from '../settings';
+import Board from './Board';
+import Paddle from './Paddle';
+
 export default class Game {
 
 	constructor(element, width, height) {
-		this.element = element;
+		this.element = element; // element = div id=game
 		this.width = width;
 		this.height = height;
-	
+		this.gameElement = document.getElementById(this.element); //creating new instance variable and querying that
 		// Other code goes here...
+		this.board = new Board(this.width, this.height);
+
+
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.boardGap = 10;
+	
+		this.player1 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			this.boardGap, // x = 10
+			((this.height - this.paddleHeight) / 2), // y = (256-56)/2 = 100
+			KEYS.a,
+			KEYS.z	
+		);
+
+		console.log(this.player1);
+
+		this.player2 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			(this.width - this.boardGap - this.paddleWidth),
+			((this.height - this.paddleHeight) / 2), // y = (256-56)/2 = 100
+			KEYS.up,
+			KEYS.down	
+		);
+		console.log(this.player2);
 	}
 
 	render() {
 		// More code goes here...
+		this.gameElement.innerHTML = ''; //prevent infinite create
+
+		let svg = document.createElementNS(SVG_NS, 'svg'); //creates svg element
+		svg.setAttributeNS(null, 'width', this.width); //basically opening <svg> tag
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+
+		this.board.render(svg);
+
+		this.player1.render(svg);
+		this.player2.render(svg);
+
+		this.gameElement.appendChild(svg);
 	}
 
 }
